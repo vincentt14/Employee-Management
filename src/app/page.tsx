4b49ac0e39b69e7f0c26166edbdec086e6abcd35
Vincent <vincent14@student.umn.ac.id>
2023-08-05 18:09:02 +0700

    rebuild

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { Footer, Navbar } from "@/components";
-import HomePage from "@/pages/HomePage";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <main>
-        <HomePage />
       </main>
       <Footer />
     </>
--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -3,15 +3,11 @@ import Link from "next/link";
 
 const CustomButton = ({ title, btnType, containerStyles, handleClick, textStyles, to }: CustomButtonProps) => {
   return (
-    <button type={btnType || "button"} className={`my-4 py-3 px-6 rounded-md font-semibold border ${containerStyles}`} onClick={handleClick}>
-      {to ? (
-        <Link href={to} className={`${textStyles}`}>
-          {title}
-        </Link>
-      ) : (
+    <Link href={to}>
+      <button type={btnType || "button"} className={`my-4 py-3 px-6 rounded-md font-semibold border ${containerStyles}`} onClick={handleClick}>
         <span className={`${textStyles}`}>{title}</span>
-      )}
-    </button>
+      </button>
+    </Link>
   );
 };
 
