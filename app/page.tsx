57f77df0c62ef49b005f51fbe0e036e7c7fb960e
Vincent <vincent14@student.umn.ac.id>
2023-08-06 23:35:17 +0700

    home done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+'use client'
+
+import Lottie from "lottie-react";
+
+import astro from "@/lottie/astro.json";
+import { Status } from "@/components/home/Status";
+import { Welcome } from "@/components/home/Welcome";
+
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      dsads
-    </div>
+    <main className="container">
+      <section id="home" className="pt-28 pb-24 lg:pt-44 lg:pb-32">
+        <div className="flex flex-col lg:flex-row">
+          <div className="w-full self-center md:px-4 lg:w-1/2">
+            <Welcome />
+            <Status />
+          </div>
+          <div className="w-full px-4 pt-4 self-center lg:w-1/2 border-2 shadow-lg border-borderColor rounded-md bg-secondary">
+            <Lottie animationData={astro} className="mx-auto blur-none h-[300px] w-[300px] lg:h-[350px] lg:w-[350px]" />
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
